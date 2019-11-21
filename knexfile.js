716c04cefc12dacc4ceb1a5a75345482932140ed
Uzias Rivera <uziasrivera@gmail.com>
2019-11-20 19:29:53 -0800

    adds pg support

--- knexfile.js
+++ knexfile.js
@@ -52,19 +52,18 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',//pg
-    connection: { //process.env.DATABASE_URL
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',//pg
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
   }
 
 };
